3c5ba9c94f42d7728ef5c5e4dfda6b2f025e2fa4
Marie-Louise Grognet <marielouise.grognet@gmail.com>
2025-04-08 17:57:19 +0200

    récupération et affichage des données du fichier JSON sur la page d'accueil

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,30 +1,16 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+async function getPhotographers() {
+    try {
+        const response = await fetch("data/photographers.json");
+        const data = await response.json(); // transforme la réponse en objet JS
+        return {
+            photographers: data.photographers
+        };
+    } catch (error) {
+        console.error("Erreur lors du chargement des photographes :", error);
+        return { photographers: [] }; // en cas d’erreur, retourne un tableau vide
     }
+}
+
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
@@ -33,6 +19,7 @@
             const photographerModel = photographerTemplate(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
             photographersSection.appendChild(userCardDOM);
+
         });
     }
 
