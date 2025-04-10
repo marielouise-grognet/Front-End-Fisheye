76d7a1a6ceac4937aa24f255fb96f688a73a6f0d
Marie-Louise Grognet <marielouise.grognet@gmail.com>
2025-04-10 14:02:44 +0200

    récupération et affichage des données Json

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,27 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/idphoto/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const location = document.createElement( 'p' );
+        location.textContent = `${city}, ${country}`;
+        const slogan = document.createElement( 'p' );
+        slogan.textContent = tagline;
+        const pricePrestation = document.createElement( 'p' );
+        pricePrestation.textContent = `${price}€/jour`;
+        console.log(price)
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(location)
+        article.appendChild(slogan)
+        article.appendChild(pricePrestation)
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, country, tagline, price, getUserCardDOM }
 }
