04b06fc35744b6f0608f0215348b04628e742762
Marie-Louise Grognet <marielouise.grognet@gmail.com>
2025-04-17 13:30:17 +0200

    début de mise en place de la modale

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -7,3 +7,25 @@ function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+
+async function displayPhotographerName() {
+    // Récupérer les données du photographe
+    const { photographer } = await getPhotographer();
+
+    // Sélectionner l'élément où le nom doit être affiché
+    const h2 = document.getElementById('contact');
+
+    if (h2 && photographer) {
+        h2.innerHTML = `Contactez-moi <br>${photographer.name}`;
+    }
+}
+
+async function init() {
+    // Mettre à jour le nom du photographe dans la modal
+    await displayPhotographerName();
+}
+
+init();
+
+
+
